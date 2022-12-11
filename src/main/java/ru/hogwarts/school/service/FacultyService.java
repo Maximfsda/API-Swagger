5d7c000aa94a9619d9f552472368c23a891e1278
Maximfsda <lisitsin.maxi@yandex.ru>
2022-12-11 20:45:09 +1000

    Commit Two

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -3,58 +3,36 @@ package ru.hogwarts.school.service;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exeptions.StudentNotFoundExeption;
 import ru.hogwarts.school.model.Faculty;
-import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
-
-    private Map<Long, Faculty>  facultys = new HashMap<>();
+    private final FacultyRepository facultyRepository;
     private Long counter = 1L;
 
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
     public Faculty createFaculty(Faculty faculty) {
-        facultys.put(counter, faculty);
-        counter++;
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     public Faculty updateFaculty(Long facultyId,Faculty faculty) {
-        if(this.facultys.containsKey(facultyId)){
-            Faculty oldFaculty = this.facultys.get(facultyId);
-            oldFaculty.setName(faculty.getName());
-            oldFaculty.setColor(faculty.getColor());
-            return oldFaculty;
-        }
-        else {
-            throw new StudentNotFoundExeption();
-        }
+        return facultyRepository.save(faculty);
     }
     public Faculty getFacultyById(Long facultyId) {
-        if(this.facultys.containsKey(facultyId)){
-            return this.facultys.get((facultyId));
-        }
-        else {
-            throw new StudentNotFoundExeption();
-        }
+        return facultyRepository.findById(facultyId).get();
     }
 
-    public Faculty deleteFaculty(Long facultyId) {
-        if(this.facultys.containsKey(facultyId)){
-            return facultys.remove(facultyId);
-        }
-        else {
-            throw new StudentNotFoundExeption();
-        }
+    public void deleteFaculty(Long facultyId) {
+        facultyRepository.deleteById(facultyId);
     }
 
     public Collection<Faculty> getByColor(String color) {
-        return this.facultys.values()
-                .stream()
-                .filter(s->s.getColor() == color)
-                .collect(Collectors.toList());
+        return facultyRepository.findAll();
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,57 +3,36 @@ package ru.hogwarts.school.service;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exeptions.StudentNotFoundExeption;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
 
-    private Map<Long, Student> students = new HashMap<>();
-    private Long counter = 1L;
+    private final StudentRepository studentRepository;
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public Student createStudent(Student student) {
-        students.put(counter, student);
-        counter++;
-        return student;
+        return studentRepository.save(student);
     }
 
-    public Student updateFaculty(Long studentId,Student student) {
-        if(this.students.containsKey(studentId)){
-            Student oldStudent = this.students.get(studentId);
-            oldStudent.setName(student.getName());
-            oldStudent.setAge(student.getAge());
-            return oldStudent;
-        }
-        else {
-            throw new StudentNotFoundExeption();
-        }
-    }
+
     public Student getStudentById(Long studentId) {
-        if(this.students.containsKey(studentId)){
-            return this.students.get((studentId));
-        }
-        else {
-            throw new StudentNotFoundExeption();
-        }
+        return studentRepository.findById(studentId).get();
+    }
+    public Student updateFaculty(Long studentId,Student student) {
+        return studentRepository.save(student);
     }
 
-    public Student deleteStudent(Long studentId) {
-        if(this.students.containsKey(studentId)){
-            return students.remove(studentId);
-        }
-        else {
-            throw new StudentNotFoundExeption();
-        }
+    public void deleteStudent(Long studentId) {
+        studentRepository.deleteById(studentId);
     }
 
     public Collection<Student> getByAge(int age) {
-        return this.students.values()
-                .stream()
-                .filter(s->s.getAge() == age)
-                .collect(Collectors.toList());
+        return studentRepository.findAll();
     }
 }
